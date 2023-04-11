49bf1f307fb4814d16cdfec118dfe430aacb86d8
User221 <a.ndre.221@yandex.ru>
2023-04-12 01:52:15 +0300

    FinalDone

--- task_final/solution.js
+++ task_final/solution.js
@@ -18,36 +18,23 @@
 //     - count - количество в заказе
 // например:
 // {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв. 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
+//    "data": { "client": "Иван +7(987)65-43-210","order": {"address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв. 53","sum": 900 },"goods": [{"title": "Пицца","count": 2}]}}
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {"client": {name, phone}, "order": {"address": address, "sum": sum}, "goods":[]}
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name + " " + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
